7c6eb5dd00eddfbd24db1c50b7c06eb4266a0f52
arunkumar-focusbrands <con_akumar01@focusbrands.com>
2025-12-31 13:38:21 +0530

    remove client

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -5,9 +5,11 @@ export const projectId = "projectId";
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-01";
 
-export const client = createClient({
-  projectId,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-01", // YYYY-MM-DD
-  useCdn: false,
-});
+export const client = {};
+
+// createClient({
+//   projectId,
+//   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+//   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-01", // YYYY-MM-DD
+//   useCdn: false,
+// });
