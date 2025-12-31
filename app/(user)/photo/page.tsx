7f64dcc6320104737f316324866323a4a91c9d2b
Arunkumar <46481890+akpgaa@users.noreply.github.com>
2025-12-31 13:04:02 +0530

    Update page.tsx

--- app/(user)/photo/page.tsx
+++ app/(user)/photo/page.tsx
@@ -1,33 +1,42 @@
-
 "use client";
 import React, { useState } from "react";
 import PresetCard from "../../components/PresetCard";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import vol1 from "../../../public/images/fflogo.jpg";
- const socialLinks = {
+
+const socialLinks = {
   url: "/",
   instagram: "https://www.instagram.com/freeze_frame_studio/",
   maps: "https://maps.app.goo.gl/h9dYCV8drRKAxi4VA",
 };
+
 function PhotoAlbum() {
   const router = useSearchParams();
   const [name, url] = [router?.get("name"), router?.get("url")];
   const [isURL, setIsURL] = useState(false);
   const text = isURL ? "See Your Photos" : "Follow Us To View Photos";
-  const origin =
-    typeof window !== "undefined" && window.location.origin
-      ? window.location.origin
-      : "";
 
-  let qrLink = "";
-  // Client-side-only code
-  if (origin) {
-    qrLink = `${origin}/photo/${name}?url=${url}`;
-  }
-
-  const newUrl = isURL ? qrLink : socialLinks.instagram;
+  const newUrl = isURL ? url : socialLinks.instagram;
   return (
-    
+    <div className="w-full px-4 lg:px-8">
+      <h1 className="font-ovo text-5xl mb-8 mt-5">Photos</h1>
+
+      <div className="grid xl:grid-cols-2 xl:space-x-2">
+        <PresetCard
+          downloadLocation="image"
+          staticImage={vol1}
+          buttonText={text}
+          url={newUrl ?? ""}
+          isURL={isURL}
+          handleClick={() => setIsURL(true)}
+          title={name ?? ""}
+          subtitle="Best Photography"
+          description={`Here is your photo's link—click on it to access your pictures. We hope you enjoy the captured moments! For more inspiration,
+      delve into the world of photography with our curated collection. Click to get lost in the art and discover insightful photography 
+      quotes that capture the essence of the craft. Happy exploring!`}
+        />
+      </div>
+    </div>
   );
 }
 
