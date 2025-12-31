4d5f2d4549d5b4f569355e20843a3366d3c16c4c
arunkumar-focusbrands <con_akumar01@focusbrands.com>
2025-12-31 13:32:47 +0530

    test build

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,12 +1,13 @@
-import { createClient } from 'next-sanity';
+import { createClient } from "next-sanity";
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-const apiVersion = process.env.NEXT_PUBLIC_SANTIY_API_VERSION;
+export const projectId = "projectId";
+//  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-01";
 
 export const client = createClient({
   projectId,
-  dataset,
-  apiVersion,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-01", // YYYY-MM-DD
   useCdn: false,
 });
