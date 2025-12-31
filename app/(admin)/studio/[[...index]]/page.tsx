ca37074af86e76418637a5cb4d92f3318d873b64
arunkumar-focusbrands <con_akumar01@focusbrands.com>
2025-12-31 13:55:29 +0530

    comm

--- app/(admin)/studio/[[...index]]/page.tsx
+++ app/(admin)/studio/[[...index]]/page.tsx
@@ -1,10 +1,11 @@
-'use client';
+"use client";
 
-import { NextStudio } from 'next-sanity/studio';
+// import { NextStudio } from 'next-sanity/studio';
 
-import config from '../../../../sanity.config';
+import config from "../../../../sanity.config";
 
 export default function StudioPage() {
+  return null;
   //  Supports the same props as `import {Studio} from 'sanity'`, `config` is required
-  return <NextStudio config={config} />;
+  // return <NextStudio config={config} />;
 }
