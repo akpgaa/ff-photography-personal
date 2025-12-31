6b4ba8b8aa7bf88949a3d307163baec149e5cace
arunkumar-focusbrands <con_akumar01@focusbrands.com>
2025-12-31 13:55:00 +0530

    ca

--- app/(user)/works/page.tsx
+++ app/(user)/works/page.tsx
@@ -1,21 +1,22 @@
-import { client } from '../../../lib/sanity.client';
-import { groq } from 'next-sanity';
-import MasonryGallery from '../../components/MasonryGallery';
-import urlFor from '../../../lib/urlFor';
+// import { client } from "../../../lib/sanity.client";
+// import { groq } from 'next-sanity';
+import MasonryGallery from "../../components/MasonryGallery";
+import urlFor from "../../../lib/urlFor";
 
-const query = groq`
-  *[_type == "imageCollection"]
-`;
+// const query = groq`
+//   *[_type == "imageCollection"]
+// `;
 
 export default async function Works() {
-  const collections = await client.fetch(query);
-  console.log(collections);
-  return (
-    <div className='px-4 lg:px-8'>
-      <h1 className='font-ovo text-5xl mb-8 mt-5'>Works</h1>
-      <div className='border-b rounded w-2/6 mx-auto my-8'></div>
+  return null;
+  // const collections = await client.fetch(query);
+  // console.log(collections);
+  // return (
+  //   <div className='px-4 lg:px-8'>
+  //     <h1 className='font-ovo text-5xl mb-8 mt-5'>Works</h1>
+  //     <div className='border-b rounded w-2/6 mx-auto my-8'></div>
 
-      <MasonryGallery collections={collections} />
-    </div>
-  );
+  //     <MasonryGallery collections={collections} />
+  //   </div>
+  // );
 }
--- app/components/PreviewBlogList.tsx
+++ app/components/PreviewBlogList.tsx
@@ -1,14 +1,14 @@
-'use client';
+"use client";
 
-import { usePreview } from '../../lib/sanity.preview';
-import BlogList from './BlogList';
+// import { usePreview } from '../../lib/sanity.preview';
+import BlogList from "./BlogList";
 
 type Props = {
-    query: string;
+  query: string;
 };
 
-
-export default function PreviewBlogList({ query}: Props) {
-    const posts = usePreview(null, query);
-    return <BlogList posts={posts} />
+export default function PreviewBlogList({ query }: Props) {
+  // const posts = usePreview(null, query);
+  // return <BlogList posts={posts} />
+  return null;
 }
